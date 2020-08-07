39e19d53445eb4032890862dd34b784ed7c73b12
EdivaldoPereira <edivaldo.pereira@gmail.com>
2020-08-07 18:58:03 -0300

    Adicionei arquivos via upload

---
+++ primeiro-projeto-java/src/primeiroProg/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package primeiroProg;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é o método auto executável em java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em Java");
+	}
+}
